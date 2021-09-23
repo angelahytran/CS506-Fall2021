7829e22fc8c4757524871e34f8bd73b5247f0386
Angela Tran <angela.tran1220@gmail.com>
2021-09-23 10:16:01 -0400

    built lake and park

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,3 +1,18 @@
 def draw_park():
     print("park not found")
+    print("                                                                                                       oxoxoo    ooxoo      ")
+    print("                                                                                                     ooxoxo oo  oxoxooo     ")
+    print("                                                                                                    oooo xxoxoo ooo ooox    ")
+    print("                                                                                                    oxo o oxoxo  xoxxoxo    ")
+    print("                                                                                                      oxo xooxoooo o ooo    ")
+    print("                   ================================                                                    ooo\oo\  /o/o        ")
+    print("                  /    |      |        |      |    \                                                       \  \/ /          ")
+    print("                 /     |      |        |      |     \                                                       |   /           ")
+    print("                /      |      |        |      |      \                                                      |  |            ")
+    print("               /       |swings|        |swings|       \                 ________bench________               |  |            ")
+    print("              /       ==========      ==========       \                |___________________|               |  |            ")
+    print("             /                                          \                 | |          | |                  |  |            ")
+    print("----------------------------------------------------------------------------------------------------------------------------")
     return
+
+                                                                    
